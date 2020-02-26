920348603e31c9b41a20f32d510c7d4930409895
gantaa <43763136+reddyashish@users.noreply.github.com>
2020-02-26 15:18:28 -0500

    CsharpPatternMatchingCheckpoint

---
+++ Training 3.9/CsharpPatternMatchingCheckpoint.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CsharpPatternMatchingCheckpoint
+{
+    [TestClass]
+    public class TestsBeforePatternMatching
+    {
+
+        [TestMethod]
+        public void Test1()
+        {
+            //Arrange
+            var passingStudent = new Student("Jane", "Doe", true, 'B');
+            var failingStudent = new Student("Jane", "Doe", true, 'D');
+
+            //Act
+            bool passResult = getPassOrFail_ByPropertyPatternMatching(passingStudent);
+            bool failResult = getPassOrFail_ByPropertyPatternMatching(failingStudent);
+
+            //Assert
+            Assert.AreEqual(true, passResult && !failResult);
+        }
+
+        [TestMethod]
+        public void Test2()
+        {
+            //Arrange
+            var passingStudent = new Student("Jane", "Doe", true, 'B');
+            var failingStudent = new Student("Jane", "Doe", true, 'D');
+
+            //Act
+            bool passResult = getPassOrFail_ByTuplePatternMatching(passingStudent);
+            bool failResult = getPassOrFail_ByTuplePatternMatching(failingStudent);
+
+            //Assert
+            Assert.AreEqual(true, passResult && !failResult);
+        }
+
+        [TestMethod]
+        public void Test3()
+        {
+            //Arrange
+            var passingStudent = new Student("Jane", "Doe", true, 'B');
+            var failingStudent = new Student("Jane", "Doe", true, 'D');
+
+            //Act
+            bool passResult = getPassOrFail_ByPositionalPatternMatching(passingStudent);
+            bool failResult = getPassOrFail_ByPositionalPatternMatching(failingStudent);
+
+            //Assert
+            Assert.AreEqual(true, passResult && !failResult);
+        }
+
+        public static bool getPassOrFail_ByPropertyPatternMatching(Student student)
+        {
+            // Use property pattern matching to determine if Grade is pass (A,B,C) or fail (D,F) and TuitionPaid is true.
+            return student switch
+            {
+                { Grade: 'A', TuitionPaid : true} => true,
+                { Grade: 'B', TuitionPaid: true } => true,
+                { Grade: 'C', TuitionPaid: true } => true,
+                { Grade: 'D', TuitionPaid: true } => false,
+                { Grade: 'F', TuitionPaid: true } => false,
+                _ => false
+            };
+        }
+
+        public static bool getPassOrFail_ByTuplePatternMatching(Student student)
+        // Use tuple pattern matching to determine if Grade is pass (A,B,C) or fail (D,F) and TuitionPaid is true.
+        => (student.TuitionPaid, student.Grade) switch
+        {
+            (true, 'A') => true,
+            (true, 'B') => true,
+            (true, 'C') => true,
+            (true, 'D') => false,
+            (true, 'E') => false,
+            (_, _) => false
+        };
+
+
+        public static bool getPassOrFail_ByPositionalPatternMatching(Student student)
+        {
+            // Use positional pattern matching to determine if Grade is pass (A,B,C) or fail (D,F) and TuitionPaid is true.
+            // Note: You will to define the Deconstruct method for Student
+            return student switch
+            {
+                var (firstName, lastName, tuitionPaid, grade) when grade == 'A' && tuitionPaid == true => true,
+                var (firstName, lastName, tuitionPaid, grade) when grade == 'B' && tuitionPaid == true => true,
+                var (firstName, lastName, tuitionPaid, grade) when grade == 'C' && tuitionPaid == true => true,
+                var (firstName, lastName, tuitionPaid, grade) when grade == 'D' && tuitionPaid == true => false,
+                var (firstName, lastName, tuitionPaid, grade) when grade == 'F' && tuitionPaid == true => false,
+                _ => false
+            };
+        }
+    }
+
+    public class Student
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public bool TuitionPaid { get; set; }
+        public char Grade { get; set; }
+
+        public Student(string firstName, string lastName, bool tuitionPaid, char grade) =>
+            (FirstName, LastName, TuitionPaid, Grade) = (firstName, lastName, tuitionPaid, grade);
+
+        public void Deconstruct(out string firstName, out string lastName, out bool tuitionPaid, out char grade) =>
+            (firstName, lastName, tuitionPaid, grade) = (FirstName, LastName, TuitionPaid, Grade);
+    }
+}
--- Training 3.9/Program.cs
+++ Training 3.9/Program.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
+using CsharpIndicesRangesCheckpoint;
+using CsharpPatternMatchingCheckpoint;
 using Microsoft.Extensions.Configuration;
 
 namespace Training_3._9
@@ -15,7 +18,7 @@ namespace Training_3._9
             ["Window:Height"] = "30",
             ["Window:Width"] = "30",
         };
-        static public IConfiguration Configuration { get; set; }
+        static public IConfiguration? Configuration { get; set; }
 
         public class Person
         {
@@ -24,6 +27,48 @@ namespace Training_3._9
             public string? Email { get; set; }
             public string? PayRate { get; set; }
         }
+
+        public enum Rainbow
+        {
+            Red,
+            Orange,
+            Yellow,
+            Green,
+            Blue,
+            Indigo,
+            Violet
+        }
+
+        public static int FromRainbow(Rainbow colorBand) =>
+        colorBand switch
+        {
+            Rainbow.Red => Color.Red.ToArgb(),
+            Rainbow.Orange => Color.Orange.ToArgb(),
+            Rainbow.Yellow => Color.Yellow.ToArgb(),
+            Rainbow.Green => Color.Green.ToArgb(),
+            Rainbow.Blue => Color.Blue.ToArgb(),
+            Rainbow.Indigo => Color.Indigo.ToArgb(),
+            Rainbow.Violet => Color.Violet.ToArgb(),
+            _ => throw new ArgumentException(message: "invalid enum value", paramName: nameof(colorBand)),
+        };
+
+        public class Address
+        {
+            public string? Street { get; set; }
+            public string? City { get; set; }
+            public string? State { get; set; }
+            public string? Zip { get; set; }
+        }
+
+        public static decimal ComputeSalesTax(Address location, decimal salePrice) =>
+        location switch
+        {
+            { State: "WA" } => salePrice * 0.06M,
+            { State: "MN" } => salePrice * 0.75M,
+            { State: "MI" } => salePrice * 0.05M,
+            { State: "TX" } => salePrice * 0.08M,
+            _ => 0M
+        };
 
         static void Main(string[] args)
         {
@@ -51,7 +96,31 @@ namespace Training_3._9
                 FirstName = "Joe"
             };
 
-            Console.WriteLine($"length: {person.FirstName.Length + person.LastName!.Length}");
+           // Console.WriteLine($"length: {person.FirstName.Length + person.LastName!.Length}");
+
+
+            TestsBefore testsBefore = new TestsBefore();
+            string[] result = testsBefore.getGradeRange(1, 3);
+
+            foreach (var word in result)
+            {
+                Console.WriteLine(word);
+            }
+
+            Console.WriteLine(FromRainbow(Rainbow.Blue));
+
+            var locationObject = new Address() { City="Deluth", State = "TX", Zip="12345" };
+            var result1 = locationObject is { State: "MN" };  // <-- Sets result to True
+            Console.WriteLine(result1);
+            result1 = locationObject is { State: "TX" };
+            Console.WriteLine(result1);
+
+            Console.WriteLine(ComputeSalesTax(locationObject,10.00M)) ;
+
+            var passingStudent = new Student("Jane", "Doe", true, 'C');
+            bool passResult = TestsBeforePatternMatching.getPassOrFail_ByPropertyPatternMatching(passingStudent);
+            Console.WriteLine("Pass Result");
+            Console.WriteLine(passResult);
 
             Console.ReadKey();
         }
