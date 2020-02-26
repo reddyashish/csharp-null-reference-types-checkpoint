56b6af265e7831ad181a70e4de90cfc987b3cd73
gantaa <43763136+reddyashish@users.noreply.github.com>
2020-02-26 13:30:07 -0500

    CsharpIndicesRangesCheckpoint

---
+++ Training 3.9/CsharpIndicesRangesCheckpoint.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CsharpIndicesRangesCheckpoint
+{
+    [TestClass]
+    public class TestsBefore
+    {
+        readonly string[] GRADES = new string[]
+            {
+                "A", "B", "C", "D", "F"
+            };
+
+        [TestMethod]
+        public void GetGradeFromEnd_WhenCalledWithValidNumber_ShouldReturnGradeFromEnd()
+        {
+            string result = getGradeFromEnd(4);
+
+            Assert.AreEqual("B", result);
+        }
+
+        public string getGradeFromEnd(int indexFromEnd)
+        {
+            string item = GRADES[^4];
+            return item;
+        }
+
+        [TestMethod]
+        public void GetGradeRange_WhenCalledWithStartAndEndIndex_ShouldReturnGradeRange()
+        {
+            string[] result = getGradeRange(1, 3);
+
+            CollectionAssert.AreEqual(new string[] { "B", "C", "D" }, result);
+        }
+
+        public string[] getGradeRange(int startIndex, int endIndex)
+        {
+            var lastItem = endIndex + 1;
+            var result = GRADES[startIndex..lastItem];
+            return result;
+        }
+    }
+}
